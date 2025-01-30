65a590bc45b97d2272ce80605fc959b80d171953
Alexanderrr123193 <reshetnikovalexander1992@gmail.com>
2025-01-30 21:39:06 +0300

    add fix111111

--- src/test/java/data/TestData.java
+++ src/test/java/data/TestData.java
@@ -1,10 +1,6 @@
 package data;
-import io.github.cdimascio.dotenv.Dotenv;
-public class TestData {
-    private static final Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
-    public static final String bookStoreLogin = dotenv.get("bookStoreLogin");
-    public static final String bookStorePassword = dotenv.get("bookStorePassword");
-    public static final String testBookIsbn = "9781449365035";
 
-    public static final String bookName = "Speaking JavaScript";
+public class TestData {
+    protected static final String bookStoreLogin = System.getProperty("bookStoreLogin");
+    protected static final String bookStorePassword = System.getProperty("bookStorePassword");
 }
