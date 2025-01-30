951e0f95eca1cc893cf7ede4c6e531881be18c98
Alexanderrr123193 <reshetnikovalexander1992@gmail.com>
2025-01-30 21:40:46 +0300

    add fix11111123333333333333

--- src/test/java/data/TestData.java
+++ src/test/java/data/TestData.java
@@ -3,4 +3,7 @@ package data;
 public class TestData {
     protected static final String bookStoreLogin = System.getProperty("bookStoreLogin");
     protected static final String bookStorePassword = System.getProperty("bookStorePassword");
+    public static final String testBookIsbn = "9781449365035";
+
+    public static final String bookName = "Speaking JavaScript";
 }
